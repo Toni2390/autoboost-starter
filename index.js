fd7da84e45c52579e7b91c8a3baa8451a5fd1c6f
Toni2390 <sultoni.agustus@gmail.com>
2025-07-06 16:52:23 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('AutoBoost Shopee is running!');
+  res.send('AutoBoost Shopee is running!');
+});
+
+// 🔥 Route callback Shopee akan arahkan ke sini
+app.get('/callback', (req, res) => {
+  console.log('Shopee callback query:', req.query);  // untuk lihat di log
+  res.send('Authorization callback received!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
