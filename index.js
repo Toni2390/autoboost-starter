b791d3220782c17af96d7b5566e9b31869d84358
Toni2390 <sultoni.agustus@gmail.com>
2025-07-07 08:17:37 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/login', (req, res) => {
-  const partner_id = process.env.TEST_PARTNER_ID;
-  const partner_key = process.env.TEST_API_PARTNER_KEY;
+  const partner_id = process.env.PARTNER_ID;
+  const partner_key = process.env.PARTNER_KEY;
   const redirect_url = process.env.REDIRECT_URL;
 
   const timestamp = Math.floor(Date.now() / 1000);
   const path = '/api/v2/shop/auth_partner';
 
-  // ✅ SIGN = HMAC(partner_key, partner_id + path + timestamp)
   const baseString = `${partner_id}${path}${timestamp}`;
   const sign = crypto.createHmac('sha256', partner_key).update(baseString).digest('hex');
 
-  const redirect = `https://partner.test-stable.shopeemobile.com${path}?partner_id=${partner_id}&timestamp=${timestamp}&sign=${sign}&redirect=${encodeURIComponent(redirect_url)}`;
+  const redirect = `https://partner.shopeemobile.com${path}?partner_id=${partner_id}&timestamp=${timestamp}&sign=${sign}&redirect=${encodeURIComponent(redirect_url)}`;
   
   res.redirect(redirect);
 });
