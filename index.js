b5114b50604c7784dcccff1ad19643d6c1f2a2dd
Toni2390 <sultoni.agustus@gmail.com>
2025-07-09 06:35:37 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -10,24 +10,23 @@ app.get('/login', (req, res) => {
 
   const timestamp = Math.floor(Date.now() / 1000);
   const path = '/api/v2/shop/auth_partner';
-
   const baseString = `${partner_id}${path}${timestamp}`;
   const sign = crypto.createHmac('sha256', partner_key).update(baseString).digest('hex');
 
-  const redirect = `https://partner.shopeemobile.com${path}?partner_id=${partner_id}&timestamp=${timestamp}&sign=${sign}&redirect=${encodeURIComponent(redirect_url)}`;
+  const authUrl = `https://partner.test-stable.shopeemobile.com${path}?partner_id=${partner_id}&timestamp=${timestamp}&sign=${sign}&redirect=${encodeURIComponent(redirect_url)}`;
   
-  res.redirect(redirect);
-});
-
-app.get('/', (req, res) => {
-  res.send('AutoBoost Shopee is running!');
+  res.redirect(authUrl);
 });
 
 app.get('/callback', (req, res) => {
-  console.log('Shopee callback query:', req.query);
+  console.log('Shopee callback:', req.query); // token, shop_id, dll
   res.send('Authorization callback received!');
 });
 
+app.get('/', (req, res) => {
+  res.send('Shopee API App is running!');
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
